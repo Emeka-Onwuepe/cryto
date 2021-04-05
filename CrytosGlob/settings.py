9906dccbac046d02574c479096891cec209889f2
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2021-04-05 09:05:37 +0100

    fix: paths corrections

--- CrytosGlob/settings.py
+++ CrytosGlob/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY =  os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["crytosglob.herokuapp.com"]
+ALLOWED_HOSTS = ["crytosglob.herokuapp.com",'127.0.0.1']
 
 
 # Application definition
